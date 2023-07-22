d0884045abb47b7c8669584b12195073783e4a4b
Hiro Mia <hiromia006@gmail.com>
2023-07-22 22:10:27 +0600

    first commit

---
+++ src/test/java/selenium/class1/BrowserLaunchTest.java
@@ -0,0 +1,19 @@
+package selenium.class1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserLaunchTest {
+    public static void main(String[] args) {
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+
+        driver.quit();
+
+
+    }
+}
