5f54518100b4a38c1a81f3a0c45927023624d7e9
Hiro Mia <hiromia006@gmail.com>
2023-08-02 22:35:38 +0600

    Selenium 6th class

---
+++ src/test/java/selenium/class6/TestNgTest.java
@@ -0,0 +1,37 @@
+package selenium.class6;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestNgTest {
+    @Test
+    public void testCases() {
+        Assert.assertEquals("10", "10");
+    }
+
+    @Test
+    public void testCases2() {
+        Assert.assertEquals("10", "10");
+    }
+
+    @Test
+    public void testCases3() {
+        Assert.assertEquals("10", "10");
+    }
+
+    @Test
+    public void loginShouldSucceed(){
+
+    }
+
+    @Test
+    public void loginShouldFail(){
+
+    }
+
+    @Test
+    public void validUsernameField(){
+
+    }
+
+}
